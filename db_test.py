1eb339ca4368e40c44d151cc465498a8f9147198
AjinkyaZ <ajinkya.zadbuke@gmail.com>
2016-01-31 18:16:31 +0530

    cleaned up main files

--- db_test.py
+++ db_test.py
@@ -4,44 +4,43 @@ import string
 import math
 from textrank import *
 
-client = pm.MongoClient('localhost', 3001)
-db = client.feeds_database
-articles = db.articles
-print "collections:"
-for collection in db.collection_names():
-    print "---", collection
 
-print ""
-article_list = articles.find()
-article_list = sorted(article_list)
-aggregate_text = ""
-for article in article_list:
-    print "********************************************"
-    print article['ID'], " : "
-    print "Title : ", article['Title']
-# this gets a list containing the body text
-    body_text = article['Body']
-# since list output cannot be  formatted as string, we join list elements
-# i.e paragraphs to create text
-    body_text_str = ' '.join(body_text)
-# unicode chars throw up probs in python 2.7 (for display), hence the encoding
-    body_text_final = body_text_str.encode("utf-8")
-    aggregate_text = aggregate_text + str(body_text_final)
-    print "Body : ", body_text_final
-    """textrank_res = textrank(body_text_final)
-    summary_indices = textrank_res[0]
-    summary_sents = textrank_res[1]
-    summary = ""
-    len_summ = 5
-    for i in range(len_summ):
-        index = summary_indices[i]
-        summary += summary_sents[index]"""
-    print "--------------------------------"
-    summary = gen_summary(body_text_final)
-    print "Summary : ", summary
-    print "--------------------------------"
-    print "Link : ", article['Link']
-    print "Source : ", article['Source']
-    print "Image : ", article['Image']
-    print "Date : ", article['Date']
+def main():
+    client = pm.MongoClient('localhost', 3001)
+    db = client.feeds_database
+    articles = db.articles
+    print "collections:"
+    for collection in db.collection_names():
+        print "---", collection
+
     print ""
+    article_list = articles.find()
+    article_list = sorted(article_list)
+    aggregate_text = ""
+    for article in article_list:
+        print "********************************************"
+        print article['ID'], " : "
+        print "Title : ", article['Title']
+        # this gets a list containing the body text
+        body_text = article['Body']
+        # since list output cannot be  formatted as string, we join list elements
+        # i.e paragraphs to create text
+        body_text_str = ' '.join(body_text)
+        # unicode chars throw up probs in python 2.7 (for display), hence the encoding
+        body_text_final = body_text_str.encode("utf-8")
+        aggregate_text = aggregate_text + str(body_text_final)
+        print "Body : ", body_text_final
+        print "--------------------------------"
+        summary = gen_summary(body_text_str)
+        #summary = summary.decode("utf-8")
+        print "Summary : ", summary
+        print "--------------------------------"
+        print "Link : ", article['Link']
+        print "Source : ", article['Source']
+        print "Image : ", article['Image']
+        print "Date : ", article['Date']
+        print ""
+
+
+if __name__ == "__main__":
+	main()
--- feeds_test.py
+++ feeds_test.py
@@ -7,31 +7,16 @@ import pymongo as pm
 from hashlib import md5
 import textrank
 
-# News sources
-#cnn_top = feedparser.parse('http://rss.cnn.com/rss/edition.rss')
-cnn_world = feedparser.parse('http://rss.cnn.com/rss/edition_world.rss')
-cnn_tech = feedparser.parse('http://rss.cnn.com/rss/edition_technology.rss')
-reuters_top = feedparser.parse('http://feeds.reuters.com/reuters/topNews')
-#cnn_sport = feedparser.parse('http://rss.cnn.com/rss/edition_sport.rss')
 
-# Main code starts here
-src_feed = cnn_world
-len_src_feed = len(src_feed['entries'])
-print "No. of articles in source feed", len_src_feed
-print "Enter no. of articles to parse, must be less than", len_src_feed
-n = int(raw_input())
-date_now = datetime.now()
-date_now = date_now.strftime('%Y-%m-%d')
-
-titles = {}
-article_text = {}
-start = datetime.now()
-
-
-def todb_article():
-    for article_num in range(n):
+def todb_article(source_feed, num_articles):
+    titles = {}
+    article_text = {}
+    date_now = datetime.now()
+    date_now = date_now.strftime('%Y-%m-%d')
+    
+    for article_num in range(num_articles):
         print "fetching data -- link", (article_num+1)
-        article_link = src_feed['entries'][article_num]['link']
+        article_link = source_feed['entries'][article_num]['link']
         article_main = urllib2.urlopen(article_link).read()
         parsed_article = bs4.BeautifulSoup(article_main, 'html.parser')
         titles[article_num] = parsed_article.h1.text
@@ -40,7 +25,8 @@ def todb_article():
         if "cnn.com" in article_link:
             article_src = "CNN"
             try:
-                image_set = parsed_article.find('img', attrs={'class':'media__image'})
+                image_set = parsed_article.find(
+                    'img', attrs={'class': 'media__image'})
                 image_src = image_set['data-medium-src']
             except KeyError, e:
                 image_src = 'dota.jpg'
@@ -58,7 +44,7 @@ def todb_article():
                             for x in container.findAllNext("p")]
             image_src = 'dota.jpg'
 
-        if len(article_body)<12:  #article is too short
+        if len(article_body) < 12:  # article is too short
             print "length of article insufficient"
             continue
 
@@ -66,8 +52,8 @@ def todb_article():
         #body_text_final = body_text_str.encode("utf-8")
         article_summary = textrank.gen_summary(body_text_str)
         # for i in range(15):
-        #	article_text[article_num][i]=parsed_article.p.text
-        #	print article_text[article_num][i]
+        #   article_text[article_num][i]=parsed_article.p.text
+        #   print article_text[article_num][i]
 
         yield {
             'ID': article_id,
@@ -80,18 +66,40 @@ def todb_article():
             'Date': date_now
         }
 
-client = pm.MongoClient('localhost', 3001)
-db = client.feeds_database
-articles = db.articles
-db.articles.remove({})
-print db.collection_names()
-#print "Source Feed ", src_feed
-for article in todb_article():
-    article_id = articles.insert_one(article).inserted_id
-    print "inserting into db"
-    print article['ID']
-    print article['Title']
-    print article['Image']
-    # print article_id
-    # print articles.find_one(article_id)
-print datetime.now()-start
+
+def main():
+    # News sources
+    #cnn_top = feedparser.parse('http://rss.cnn.com/rss/edition.rss')
+    cnn_world = feedparser.parse('http://rss.cnn.com/rss/edition_world.rss')
+    cnn_tech = feedparser.parse(
+        'http://rss.cnn.com/rss/edition_technology.rss')
+    reuters_top = feedparser.parse('http://feeds.reuters.com/reuters/topNews')
+    #cnn_sport = feedparser.parse('http://rss.cnn.com/rss/edition_sport.rss')
+
+    # Main code starts here
+    src_feed = cnn_world
+    len_src_feed = len(src_feed['entries'])
+    print "No. of articles in source feed", len_src_feed
+    print "Enter no. of articles to parse, must be less than", len_src_feed
+    n = int(raw_input())
+    start = datetime.now()
+
+    client = pm.MongoClient('localhost', 3001)
+    db = client.feeds_database
+    articles = db.articles
+    db.articles.remove({})
+    print db.collection_names()
+    # print "Source Feed ", src_feed
+    for article in todb_article(src_feed, n):
+        article_id = articles.insert_one(article).inserted_id
+        print "inserting into db"
+        print article['ID']
+        print article['Title']
+        print article['Image']
+        # print article_id
+        # print articles.find_one(article_id)
+    print datetime.now()-start
+
+
+if __name__ == "__main__":
+    main()
